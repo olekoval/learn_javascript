0040df2f39f844a1f885a1d75fe36aa15f6a335a
oleh.koval <oleh.koval@nszu.gov.ua>
2025-06-13 14:40:13 +0300

    ch4 for

--- ch4/Bubblegum.js
+++ ch4/Bubblegum.js
@@ -3,10 +3,16 @@
 
 var products = ["Choo Choo Chocolate", "Icy Mint", "Cake Batter", "Bubblegum"];
 var hasBubbleGum = [false, false, false, true];
-var i = 0
-while (i < hasBubbleGum.length){
+// var i = 0
+// while (i < hasBubbleGum.length){
+//     if(hasBubbleGum[i]){
+//        console.log(products[i] + " contains bubble gum"); 
+//     }
+//     i = i + 1;
+// }
+// завдання сторінка 173 відповідь сторінка 200
+for (var i = 0; i < hasBubbleGum.length; i = i + 1) {
     if(hasBubbleGum[i]){
        console.log(products[i] + " contains bubble gum"); 
     }
-    i = i + 1;
 }
